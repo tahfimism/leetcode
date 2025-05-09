6d5d7deb9dbc67abd6e49a5a38cf6a693beeb295
Tahfim Khan <84078873+tahfimism@users.noreply.github.com>
2025-05-09 06:32:31 +0600

    Time: 0 ms (100%), Space: 8.4 MB (66.67%) - LeetHub

--- 3859-maximum-product-of-two-digits/3859-maximum-product-of-two-digits.c
+++ 3859-maximum-product-of-two-digits/3859-maximum-product-of-two-digits.c
@@ -1,33 +1,29 @@
 int maxProduct(int n)
 {
-    int num[10];
-    int temp = n;
+
+ int temp = n;
     int i = 0;
-    int p = 0;
+
+    int max1 = 0;
+    int max2 = 0;
+
     do
     {
         int digit = temp % 10;
-        temp = temp /10;
+        temp = temp / 10;
 
-        num[i] = digit;
-        i++;
+        if(digit >= max1)
+        {
+            max2 = max1;
+            max1 = digit;
+        }
+        else if(digit > max2)
+        {
+            max2 = digit;
+        }
+
     }
     while(temp > 0);
 
-    for(int j = 0; j < i; j++)
-    {
-        for(int k = 0; k < i; k++)
-        {
-
-            if(j == k)
-            {
-                continue;
-            }
-            int product = num[j] * num[k];
-            if (product > p)
-                p = product;
-        }
-    }
-
-    return p;
+    return max1*max2;
 }
