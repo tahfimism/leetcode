36453b9c55f3f502fb971fee516152bc5a6d2a1d
Nakib Noor <84078873+tahfimism@users.noreply.github.com>
2025-05-14 22:50:07 +0600

    Time: 0 ms (100%), Space: 17.3 MB (18.91%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.c
+++ 0344-reverse-string/0344-reverse-string.c
@@ -2,7 +2,7 @@ void reverseString(char* s, int sSize) {
     
     for(int i = 0; i < sSize/2; i++)
     {
-        char temp = s[i];
+        int temp = s[i];
         s[i] = s[sSize-i-1];
         s[sSize-i-1] = temp;
     }
