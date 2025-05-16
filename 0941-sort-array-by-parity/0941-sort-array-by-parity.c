161a901523646a56edb33bb0d416d0d7885eb57e
Nakib Noor <84078873+tahfimism@users.noreply.github.com>
2025-05-16 16:27:23 +0600

    Time: 0 ms (100%), Space: 13.8 MB (18.05%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.c
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.c
@@ -5,26 +5,23 @@ int* sortArrayByParity(int* nums, int numsSize, int* returnSize) {
     
     // go thru each int, if its even - move it to front, if its odd - put it into another array
     int evenpos = 0;
-    int oddpos = 0;
+    int oddpos = numsSize-1;
 
-    int odds[numsSize];
+    int *result = malloc(numsSize * sizeof(int));
 
     for (int i = 0; i < numsSize; i++)
     {
         if(nums[i] % 2 == 0){
-            nums[evenpos++] = nums[i];
+            result[evenpos++] = nums[i];
         }
         else{
-            odds[oddpos++] = nums[i];
+            result[oddpos--] = nums[i];
         }
     }
 
-    for(int i = 0; i < oddpos; i++)
-    {
-        nums[evenpos++] = odds[i];
-    }
+  
 
     *returnSize = numsSize;
-    return nums;
+    return result;
     
 }
