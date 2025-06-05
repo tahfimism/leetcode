32a9d375f70a37895344b7530acbef569a33496b
Nakib Noor <84078873+tahfimism@users.noreply.github.com>
2025-06-05 16:43:49 +0600

    Time: 19 ms (73.3%), Space: 18.2 MB (36.75%) - LeetHub

--- 2486-most-frequent-even-element/2486-most-frequent-even-element.py
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.py
@@ -10,16 +10,16 @@ class Solution:
                 freq[num] = freq.get(num, 0) + 1
                 if max_f < freq[num]:
                     max_f = freq[num]
-                    max = num
-                elif max_f == freq[num] and max > num:
-                    max = num
+                    max_num= num
+                elif max_f == freq[num] and max_num> num:
+                    max_num= num
                     
 
         
         if has_even == False:
             return -1
         
-        return max
+        return max_num
 
 
         
