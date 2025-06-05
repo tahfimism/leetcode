2b2230941613d1a5898644cc526dbfeedd6cc642
Nakib Noor <84078873+tahfimism@users.noreply.github.com>
2025-06-05 23:48:26 +0600

    Time: 2 ms (30.34%), Space: 18 MB (20.98%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.py
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.py
@@ -1,5 +1,7 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
+        if 0 in nums:
+            return 0
         def signFunc(x):
             if X == 0:
                 return 0
@@ -9,8 +11,6 @@ class Solution:
                 return -1
         product = 1
         for num in nums:
-            if num == 0:
-                return 0
             product *= num
         
         return signFunc(product)
